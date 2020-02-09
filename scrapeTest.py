1a2e3326abf6dc240617f1bd415149388bddc10d
Carriger <60496556+Carriger@users.noreply.github.com>
2020-02-08 22:49:24 -0600

    Add files via upload
    will parse through sub directories

--- scrapeTest.py
+++ scrapeTest.py
@@ -32,31 +32,35 @@ def logError(e):
     print(e)
 
 def main():
+    directoryList = os.listdir('.')  # '.' indicates the current directory
+    homeDir = os.getcwd()
+    docUnits = "DocumentUnits"
+    
     rawHTML = fetchFromURL('http://shakespeare.mit.edu')
     soup = BeautifulSoup(rawHTML, 'html.parser')
-    #wb is binary mode and makes no changes as data is written
-##    f = open("main.html", 'wb')
-##    f.write(rawHTML)
-##    f.close()
-##    f = open("main.html", "r")
-##    data = f.read()
 
     allLinks = soup.find_all('a')
     
     ################the following is for handling the play links
     
 ##    playsLinks = allLinks[2:-7]
-##    
-##    for link in playsLinks:
-##        link = link.get('href')
-##        link = link[:-11]
-##        fullTextLink = ("http://shakespeare.mit.edu/" + link + "/full.html")
-##        newHTML = fetchFromURL(fullTextLink)
-##        soup = BeautifulSoup(newHTML, 'html.parser')
-##        HTMLlink = link + ".html"
-##        fOpen = open(HTMLlink, "wb")
-##        fOpen.write(newHTML)
-##        fOpen.close()
+##    for dirItem in directoryList:
+##        if os.path.isdir(dirItem) and dirItem == docUnits:
+##            os.chdir(dirItem) #go into subdirectory Document Units
+##            
+##            for link in playsLinks:
+##                link = link.get('href')
+##                link = link[:-11]
+##                fullTextLink = ("http://shakespeare.mit.edu/" + link + "/full.html")
+##                newHTML = fetchFromURL(fullTextLink)
+##                soup = BeautifulSoup(newHTML, 'html.parser')
+##                HTMLlink = link + ".html"
+##                    
+##                fOpen = open(HTMLlink, "wb")
+##                fOpen.write(newHTML)
+##                fOpen.close()
+##            #sends us back to the parent directory
+##            os.chdir(homeDir)
 
     ############the following is for handling the SONNET links
     
@@ -65,69 +69,105 @@ def main():
 ##    allSonnetLinks = soup.find_all('a')
 ##    allSonnetLinks = allSonnetLinks[1:]
 ##
-##    for sLink in allSonnetLinks:
-##        sLink = sLink.get('href')
-##        fullSLink = ("http://shakespeare.mit.edu/Poetry/" + sLink)
-##        sonnetsHTML = fetchFromURL(fullSLink)
-##        soup = BeautifulSoup(sonnetsHTML, 'html.parser')
+##    for dirItem in directoryList:
+##        if os.path.isdir(dirItem) and dirItem == docUnits:
+##            os.chdir(dirItem) #go into subdirectory DocumentUnits
 ##
-##        sFOpen = open(sLink, "wb")
-##        sFOpen.write(sonnetsHTML)
-##        sFOpen.close()
+##            for sLink in allSonnetLinks:
+##                sLink = sLink.get('href')
+##                fullSLink = ("http://shakespeare.mit.edu/Poetry/" + sLink)
+##                sonnetsHTML = fetchFromURL(fullSLink)
+##                soup = BeautifulSoup(sonnetsHTML, 'html.parser')
+##
+##                sFOpen = open(sLink, "wb")
+##                sFOpen.write(sonnetsHTML)
+##                sFOpen.close()
+##            #sends us back to the parent directory
+##            os.chdir(homeDir)
 
     ############the following is for handling the remaining four poems
     
 ##    finalPoems = allLinks[40:-2]
-##    for fLink in finalPoems:
-##        fLink = fLink.get('href')
-##        fLink = fLink[7:]
-##        fullFLink = ("http://shakespeare.mit.edu/Poetry/" + fLink)
-##        finalHTML = fetchFromURL(fullFLink)
-##        soup = BeautifulSoup(finalHTML, 'html.parser')
 ##
-##        finalOpen = open(fLink, 'wb')
-##        finalOpen.write(finalHTML)
-##        finalOpen.close()
+##    for dirItem in directoryList:
+##        if os.path.isdir(dirItem) and dirItem == docUnits:
+##            os.chdir(dirItem) #go into subdirectory DocumentUnits
+##            for fLink in finalPoems:
+##                fLink = fLink.get('href')
+##                fLink = fLink[7:]
+##                fullFLink = ("http://shakespeare.mit.edu/Poetry/" + fLink)
+##                finalHTML = fetchFromURL(fullFLink)
+##                soup = BeautifulSoup(finalHTML, 'html.parser')
+##
+##                finalOpen = open(fLink, 'wb')
+##                finalOpen.write(finalHTML)
+##                finalOpen.close()
+##            os.chdir(homeDir)
 
 
-    ############## making the html pages into text files
+        ############## making the html pages into text files
 
-    #contents of the current working directory
+##    #contents of the current working directory
 ##    directoryList = os.listdir('.')  # '.' indicates the current directory
 ##    for dirItem in directoryList:
-##        if dirItem[-5:] == ".html":
-##            fileOpen = open(dirItem, 'r')
-##            data = fileOpen.read()
-##            fileOpen.close()
-##            #text locally, how do we tokenize it?
+##        if os.path.isdir(dirItem) and dirItem == docUnits:
+##            os.chdir(dirItem) #go into subdirectory DocumentUnits
+##            directoryList = os.listdir('.')  # '.' indicates the current directory
+##            #going through html files within DocumentUnits
+##            for dirItem in directoryList:
+##                fileOpen = open(dirItem, 'r')
+##                data = fileOpen.read()
+##                fileOpen.close()
+##                #text locally, how do we tokenize it?
+##                soup = BeautifulSoup(data, 'html.parser')
+##                extractedText = soup.get_text()
+##                extractedText = extractedText.lower()
+##                newDItem = dirItem[:-5] + '.txt'
 ##
-##            soup = BeautifulSoup(data, 'html.parser')
-##            extractedText = soup.get_text()
-##            extractedText = extractedText.lower()
-##            newDItem = dirItem[:-5] + '.txt'
-##
-##            fileOut = open(newDItem, "w")
-##            fileOut.write(extractedText)
-##            fileOut.close()
-            
-    tokenDict = {}
-    docID = 1            
+##                fileOut = open(newDItem, "w")
+##                fileOut.write(extractedText)
+##                fileOut.close()
+##            os.chdir(homeDir)
+
+        ##################iterating through text files and tokenizing
+    txtFiles = "TextFiles"
     directoryList = os.listdir('.')  # '.' indicates the current directory
     for dirItem in directoryList:
-        #isolating our text file types
-        if dirItem[-4:] == ".txt":
-            fileOpen = open(dirItem, 'r')
-            data = fileOpen.read()
-            fileOpen.close()
-            #text locally, now tokenizing
-            tokenizedText = word_tokenize(data)
-            #anything that doesn't match this char set will be excluded
-            for term in tokenizedText:
-                if len(term) >= 4:
-                    reg = re.compile('[^a-zA-Z]')
-                    term = reg.sub('', term)
-                    print(term)
-        docID += 1
+        if os.path.isdir(dirItem) and dirItem == txtFiles:
+            os.chdir(dirItem) #go into subdirectory TextFiles
+            directoryList = os.listdir('.')  # '.' indicates the current directory
+            #going through html files within DocumentUnits
+            for dirItem in directoryList:
+                tokenDict = {}
+                docID = 1            
+                #isolating our text file types
+                if dirItem == "LoversComplaint.txt":
+                    fileOpen = open(dirItem, 'r')
+                    data = fileOpen.read()
+                    fileOpen.close()
+                    #text locally, now tokenizing
+                    tokenizedText = word_tokenize(data)
+                    #anything that doesn't match this char set will be excluded
+                    for term in tokenizedText:
+                        if len(term) >= 4:
+                            reg = re.compile('[^a-zA-Z]')
+                            term = reg.sub('', term)
+                            if term not in tokenDict:
+                                tokenDict[term] = docID
+##                                else:
+##                                    temp = tokenDict[term]
+##                                    if tokenDict[term] != docID:
+##                                        tokenDict[term] = 
+                                    
+                    docID += 1
+                print(tokenDict)
+
+
+
+            
+            
+        
+
 
 
 main()
